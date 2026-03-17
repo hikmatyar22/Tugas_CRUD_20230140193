49c1bf2330959d1d6aa78edd496a66991991cd42
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-03-17 15:09:53 +0700

    Membuat [KtpRepository.java] untuk menyediakan lapisan akses data (*data access layer*) yang memungkinkan aplikasi berinteraksi langsung dengan *database* MySQL menggunakan Spring Data JPA guna menjalankan operasi kueri dan manipulasi data KTP.

---
+++ src/main/java/com/example/praktikum2/repository/KtpRepository.java
@@ -0,0 +1,13 @@
+package com.example.praktikum2.repository;
+
+import com.example.praktikum2.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+    boolean existsByNomorKtp(String nomorKtp);
+}
