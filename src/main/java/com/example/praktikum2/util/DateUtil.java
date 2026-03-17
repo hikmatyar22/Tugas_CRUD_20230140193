73873357e3eeb1f654423ce397dc076594e43ec6
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-03-17 15:13:09 +0700

    Membuat [DateUtil.java] untuk menyediakan fungsi pembantu (*utility functions*) terkait pengolahan tanggal, seperti menentukan format standar `yyyy-MM-dd`, agar pola penulisan tanggal di seluruh aplikasi tetap seragam dan konsisten.

---
+++ src/main/java/com/example/praktikum2/util/DateUtil.java
@@ -0,0 +1,7 @@
+package com.example.praktikum2.util;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+}
