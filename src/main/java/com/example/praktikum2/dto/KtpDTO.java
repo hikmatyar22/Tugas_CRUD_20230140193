009fa998d368ab7b29654597f01b298e70eef930
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-03-17 15:04:06 +0700

    Membuat KtpDTO.java untuk menjadi objek pengantar data yang dikirim antara client dan server, sehingga menjaga keamanan entitas database serta memungkinkan fleksibilitas dalam menampilkan data yang hanya dibutuhkan oleh pengguna.

---
+++ src/main/java/com/example/praktikum2/dto/KtpDTO.java
@@ -0,0 +1,18 @@
+package com.example.praktikum2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KtpDTO {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
